257d7e8095bc4170fee101ed58875662287d1033
ahmed wahdan <wahdana471@gmail.com>
2023-09-12 20:08:15 +0300

    just testing

--- MyForm.h
+++ MyForm.h
@@ -34,8 +34,11 @@ namespace joystick {
 				delete components;
 			}
 		}
+
+
+
 	private: System::Windows::Forms::Button^ button1;
-	private: System::Windows::Forms::Timer^ timer1;
+	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::ComponentModel::IContainer^ components;
 	protected:
 
@@ -52,46 +55,57 @@ namespace joystick {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->button1 = (gcnew System::Windows::Forms::Button());
-			this->timer1 = (gcnew System::Windows::Forms::Timer(this->components));
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->SuspendLayout();
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(511, 239);
+			this->button1->BackColor = System::Drawing::Color::Transparent;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Location = System::Drawing::Point(611, 362);
 			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(184, 89);
+			this->button1->Size = System::Drawing::Size(126, 43);
 			this->button1->TabIndex = 0;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			this->button1->UseVisualStyleBackColor = false;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click_1);
 			// 
-			// timer1
+			// textBox1
 			// 
-			this->timer1->Interval = 5000;
-			this->timer1->Tick += gcnew System::EventHandler(this, &MyForm::timer1_Tick);
+			this->textBox1->BackColor = System::Drawing::SystemColors::InfoText;
+			this->textBox1->ForeColor = System::Drawing::Color::Transparent;
+			this->textBox1->Location = System::Drawing::Point(403, 309);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(543, 22);
+			this->textBox1->TabIndex = 1;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
+			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(1349, 668);
+			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button1);
+			this->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)));
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
 	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
 	}
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-		this->Close();
-	}
-	private: System::Void timer1_Tick(System::Object^ sender, System::EventArgs^ e) {
-
+	
+	private: System::Void button1_Click_1(System::Object^ sender, System::EventArgs^ e) {
+		String^value = textBox1->Text;
+		MessageBox::Show(value);
 	}
 	};
 }
--- used_classes.h
+++ used_classes.h
@@ -2,7 +2,9 @@
 #include<vector>
 #include<map>
 #include<string>
-
+#include <chrono>
+#include <thread>
+#include <windows.h>
  
 
 class Products {
@@ -22,29 +24,65 @@ public:
     }
 
     // Function to add product
-    void add_product(std::string name, int money) {
+    void add_product(std::string& name, int money) {
         productPrices[name] = money;
     }
-    void delete_product(std::string name) {
+    void delete_product(std::string& name) {
         productPrices.erase(name);
    }
+    void edit_prices(std::string& name, int newprice) {
+        productPrices[name] = newprice;
+
+    }
 
 };
 
 
-class Room {
+class GameRoom {
 public:
-	int RoomId;
-	float Total;
-    std::vector<std::string>orders;
+    int RoomId;
+    float Total;
+    std::vector<std::string> orders;
+    std::chrono::steady_clock::time_point startTime;
+    std::chrono::steady_clock::time_point endTime;
+    std::map<std::string, double> orderedProducts;
+
+    GameRoom(int roomId) : RoomId(roomId), Total(0) {}
+
+    void startTimer() {
+        startTime = std::chrono::steady_clock::now();
+    }
+
+    void stopTimer() {
+        endTime = std::chrono::steady_clock::now();
+    }
+
+    void rentForFixedTime(double hours) {
+        startTimer();
+        std::this_thread::sleep_for(std::chrono::hours(static_cast<int>(hours)));
+        stopTimer();
+
+        // Display a notification message box
+        MessageBox(NULL, TEXT("Room rental is finished."), TEXT("Room Finished"), MB_ICONINFORMATION | MB_OK);
+        //Beep(300, 20000);
+    }
+
     
 
+    void calculateCost() {
+        std::chrono::duration<double> elapsed = endTime - startTime;
+        double timeInHours = elapsed.count() / 3600.0;
 
+        double roomCost = timeInHours * 50.0;
 
+        for (const auto& order : orders) {
+            if (orderedProducts.find(order) != orderedProducts.end()) {
+                roomCost += orderedProducts[order];
+            }
+        }
 
-
-
-
-
+        Total = static_cast<float>(roomCost);
+        //return roomCost;
+    }
 };
 
