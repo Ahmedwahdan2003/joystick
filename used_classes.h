5b68e95f3823d7eb6f9e1f1643991f5b9439298b
ahmed wahdan <wahdana471@gmail.com>
2023-09-13 15:40:46 +0300

    aa

--- used_classes.h
+++ used_classes.h
@@ -6,37 +6,17 @@
 #include <thread>
 #include <windows.h>
  
-class Products {
-private:
-    std::map<std::string, double> productPrices;
-
-public:
-    // Constructor to initialize the map
-
-
-    // Function to add product
-    void add_product(std::string& name, int money) {
-        productPrices[name] = money;
-    }
-    void delete_product(std::string& name) {
-        productPrices.erase(name);
-   }
-    void edit_prices(std::string& name, int newprice) {
-        productPrices[name] = newprice;
-
-    }
-
-};
 
 
 class GameRoom {
 public:
     int RoomId;
     float Total;
-    std::vector<std::string> orders;
+    float Total_hours;
+    bool available;
     std::chrono::steady_clock::time_point startTime;
     std::chrono::steady_clock::time_point endTime;
-    std::map<std::string, double> orderedProducts;
+   
 
     GameRoom(int roomId) : RoomId(roomId), Total(0) {}
 
@@ -66,11 +46,7 @@ public:
 
         double roomCost = timeInHours * 50.0;
 
-        for (const auto& order : orders) {
-            if (orderedProducts.find(order) != orderedProducts.end()) {
-                roomCost += orderedProducts[order];
-            }
-        }
+        //database query
 
         Total = static_cast<float>(roomCost);
         //return roomCost;
