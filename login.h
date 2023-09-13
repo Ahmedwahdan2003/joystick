2d7ff8dcb91bd43190f8ef38503e0a45ca42a770
ahmed wahdan <wahdana471@gmail.com>
2023-09-13 03:49:26 +0300

    hg

--- login.h
+++ login.h
@@ -40,7 +40,7 @@ namespace joystick {
 
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button1;
+
 	private: System::ComponentModel::IContainer^ components;
 	protected:
 
@@ -60,7 +60,6 @@ namespace joystick {
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(login::typeid));
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// textBox1
@@ -89,15 +88,6 @@ namespace joystick {
 			this->button2->TabIndex = 2;
 			this->button2->UseVisualStyleBackColor = false;
 			// 
-			// button1
-			// 
-			this->button1->Location = System::Drawing::Point(358, 161);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(161, 44);
-			this->button1->TabIndex = 3;
-			this->button1->Text = L"button1";
-			this->button1->UseVisualStyleBackColor = true;
-			// 
 			// login
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
@@ -105,7 +95,6 @@ namespace joystick {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(1335, 716);
-			this->Controls->Add(this->button1);
 			this->Controls->Add(this->button2);
 			this->Controls->Add(this->textBox1);
 			this->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
